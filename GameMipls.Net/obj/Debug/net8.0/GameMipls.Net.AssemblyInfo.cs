6c3befd0e66ea3962ba30d71479e74a0cc47c8ab
rgrig <r.grigorev1502@gmail.com>
2024-04-06 11:51:37 +0300

    починил options

--- GameMipls.Net/obj/Debug/net8.0/GameMipls.Net.AssemblyInfo.cs
+++ GameMipls.Net/obj/Debug/net8.0/GameMipls.Net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameMipls.Net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb122d32b9efa5b45f2376bd543a9cc9daf84f47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf30dfbff29ee52ccee3e46f2508e79ed9c11c21")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameMipls.Net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameMipls.Net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
